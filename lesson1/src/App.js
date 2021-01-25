699fa99b642e02c3e8548f7a339225e4d09c35d7
Kostya <alienmode@Mail.ru>
2021-01-25 15:48:15 +0300

    lesson2

--- lesson1/src/App.js
+++ lesson1/src/App.js
@@ -1,46 +1,73 @@
-import React from "react";
+import React, {useState,useEffect, useCallback } from "react";
 // import logo from './logo.svg';
-import Counter from './Counter';
+import Message from './Message';
+import Input from './Input';
 import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-
-//     </div>
-//   );
+// class App extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             counter: 0,
+//             message:String,
+//         };
+//     }
+//     handelIncrease = () => {
+//         this.setState((prevState) => ({
+//             counter: prevState.counter + 1,
+//         }))
+//     };
+//     mesRend = ()=>{
+//         this.setState((prevState)=>({
+//             message : [],
+//         }))
+//     };
+//     render() {
+//         return ( < div className = "Wrapper" >
+//             <Counter count = { this.state.counter} onIncrease = { this.handelIncrease }/>
+//             <Chat />
+//              </div >
+//         )
+//     };
 // }
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            counter: 0,
-        };
-    }
-    handelIncrease = () => {
-        this.setState((prevState) => ({
-            counter: prevState.counter + 1,
-        }))
-    };
-    render() {
-        return ( < div className = "Wrapper" >
-            <Counter count = { this.state.counter} onIncrease = { this.handelIncrease }/> </div >
+
+export default function App(){
+    const [messages, setMessages] = useState([
+        {
+            text:"message1", author:"me"
+        },
+        {
+            text:"message2", author:"me"
+        },
+    ]);
+
+    const renderMessage = useCallback((messages,i)=>{
+        return <Message message={messages} key ={i}  />
+    } ,[]);
+        
+        const handleAddMessage = useCallback((text,author = "me")=>{
+            setMessages((oldMessages)=>([...oldMessages,{text,author}]));
+        },[]);
+
+        useEffect(()=>{
+            let timeout;
+            if(messages[messages.length - 1].author !== 'robot'){
+                timeout = setTimeout(()=>{
+                    handleAddMessage('Ждите ответа оператора','robot')
+                },1000);
+            }
+            return ()=>{
+                clearTimeout(timeout);
+
+            }
+
+        },[messages,handleAddMessage]);
+
+        return(
+            <>
+            {messages.map(renderMessage)}
+            <Input onAddMessage = {handleAddMessage}/>
+            </>
         )
-    };
 }
 
-export default App;
+// export default App;
--- lesson1/src/Counter.js
+++ lesson1/src/Counter.js
@@ -6,6 +6,14 @@ export default class Counter extends React.Component {
             <div>
                 < span > { this.props.count} </span>
                 <button onClick={this.props.onIncrease}>Increase</button>
+                <form>
+                <input onClick = {this.props.message}></input>
+                    <button 
+                        onClick = {this.props.message}
+                    >Send
+                        </button>
+               </form>
+               
             </div>
         )
     };
